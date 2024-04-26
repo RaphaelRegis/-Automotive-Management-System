81674e81ed4f8fc02e300146ab879f3c37eb6bd8
RaphaelRegis <raph4life17@gmail.com>
2024-04-26 14:42:58 -0300

    estrutura basica do projeto

---
+++ src/main/java/com/sistemaAutomotivo/SistemaAutomotivo/SistemaAutomotivoApplication.java
@@ -0,0 +1,12 @@
+package com.sistemaAutomotivo.SistemaAutomotivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAutomotivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAutomotivoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sistemaAutomotivo/SistemaAutomotivo/SistemaAutomotivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaAutomotivo.SistemaAutomotivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAutomotivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
