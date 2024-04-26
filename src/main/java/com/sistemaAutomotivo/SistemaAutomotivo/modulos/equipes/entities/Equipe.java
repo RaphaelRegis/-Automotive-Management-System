0acd0f5db6f39e65f4355c00db30b41db38a9cb6
RaphaelRegis <raph4life17@gmail.com>
2024-04-26 14:45:53 -0300

    feature: Equipe

---
+++ src/main/java/com/sistemaAutomotivo/SistemaAutomotivo/modulos/equipes/entities/Equipe.java
@@ -0,0 +1,58 @@
+package com.sistemaAutomotivo.SistemaAutomotivo.modulos.equipes.entities;
+
+
+import java.util.List;
+
+import com.sistemaAutomotivo.SistemaAutomotivo.modulos.funcionarios.entities.Funcionario;
+import com.sistemaAutomotivo.SistemaAutomotivo.modulos.relacionamentos.entities.ServicoOrcamento;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Equipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id_equipe;
+
+    @Column(nullable = true, unique = true)
+    private String nome;
+
+    private String setor;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_responsavel", referencedColumnName = "id_funcionario")
+    private Funcionario responsavel;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+        name = "membro_equipe",	//nome da nova tabela
+        joinColumns = @JoinColumn(name = "id_equipe"), //qual coluna da entidade proprietária (a que tem o @JoinTable)
+        inverseJoinColumns = @JoinColumn(name = "id_funcionario") //qual coluna da entidade do outro lado do relacionamento
+    )
+    private List<Funcionario> membros;
+
+    @OneToMany(mappedBy = "equipe")
+    private List<ServicoOrcamento> servicos_orcamentos;
+
+}
